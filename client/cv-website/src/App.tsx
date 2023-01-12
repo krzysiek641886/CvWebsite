a14652c615819895dc34927c60e081f95be571ef
Krzysztof Mroz <kmr@trackman.com>
2023-01-12 20:55:03 +0100

    Added request from client to server

--- client/cv-website/src/App.tsx
+++ client/cv-website/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+  const pokeDB = () => {
+    Axios.post('http://localost:3001/api/poke', {foo: "bar"});
+  };
+
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <button onClick={pokeDB}>Poke database</button>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
+const mysql = require('mysql');
+
+const db = mysql.createPool({
+	host: "localhost",
+	user: "root",
+	password: "password",
+	database: "dummyDB"
+})
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
 	res.send("hello world");
+});
+
+app.post("/api/poke", (req, res) => {
+	const foo = req.body.foo;
+	const sqlInsert = 'INSERT INTO dummyTable (foo) VALUES (?) ='
+	db.query(sqlInsert, [foo], (err, result) => {
+		console.log(result);
+	});
 });
 
 app.listen(3001, () => {
