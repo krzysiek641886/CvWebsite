98212f8d4f05fb53357de6586dd1480bcd60d2ed
kmr <kmr@trackman.com>
2023-02-11 13:13:56 +0100

    Created router

--- cv-website/src/App.js
+++ cv-website/src/App.js
@@ -1,11 +1,33 @@
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./Pages/Home"
+import PageNotFound404 from "./Pages/PageNotFound404"
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      {
+        path: "Home",
+        element: <Home />
+      }
+    ],
+  },
+  {
+    path: "*",
+    element: <PageNotFound404 />
+  }
+]);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <div>Hello world</div>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ cv-website/src/Pages/PageNotFound404.js
@@ -0,0 +1,9 @@
+function PageNotFound404() {
+	return (
+	  <div className="ErrorPage">
+		<div>Page not found 404</div>
+	  </div>
+	);
+  }
+  
+  export default PageNotFound404;
