ec3a9dfc6189a3705bf12e7983eeb67564056244
Krzysztof Mroz <57630558+krzysiek641886@users.noreply.github.com>
2023-02-12 20:31:06 +0100

    Created a Router that connects App with Home page, PageNotFound and Navbar component (#2)
    * Created router
    
    * Created a skeleton for the page
    
    ---------
    
    Co-authored-by: kmr <kmr@trackman.com>

--- cv-website/src/App.js
+++ cv-website/src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from "./Pages/Home"
+import PageNotFound404 from "./Pages/PageNotFound404"
+import Navbar from "./Components/Navbar"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <div>Hello world</div>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<PageNotFound404 />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ cv-website/src/Pages/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+	return (
+	  <div className="Homepage">
+		<div>Home page</div>
+	  </div>
+	);
+  }
+  
+  export default Home;
