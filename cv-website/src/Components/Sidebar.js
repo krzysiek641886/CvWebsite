85d571e75b9b1bbf7bebaab55bc7fd4f2ad2c944
kmr <kmr@trackman.com>
2023-03-19 16:44:29 +0100

    Added loading of the image

--- cv-website/src/Components/Sidebar.js
+++ cv-website/src/Components/Sidebar.js
@@ -1,9 +1,18 @@
 import "./Sidebar.css"
+import portrait from '../Assets/Images/portrait.jpg'; // Import your image here
 
 function Sidebar() {
 	return (
 		<nav className="sidebar">
-			<h1>Sidebar</h1>
+			<h1>Look at this sexy guy!</h1>
+			<div className="portrait-component">
+				<img src={portrait} alt="My Image" />
+			</div>
+			<div className="sidebar-text">
+				<p>Interesting facts about this sexy guy!</p>
+				<p>Even more interesting facts about this sexy guy!</p>
+			</div>
+
 		</nav>
 	);
 }
