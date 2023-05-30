ca3a5f8fc77293e16755a64e811cfb1fc331de7f
krzysiek641886 <krzysiek6400@gmail.com>
2023-05-30 15:16:57 +0200

    Navbar changes into toggle button on small devices. Toggle list appears in a wrong position

--- cv-website/src/Components/Navbar.js
+++ cv-website/src/Components/Navbar.js
@@ -1,16 +1,28 @@
+import React, { useState } from "react";
 import "./Navbar.css";
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
 function Navbar() {
-    return (
-        <nav className="nav">
-            <ul>
-                <CustomLink to="/WorkExperience">WorkExperience</CustomLink>
-                <CustomLink to="/Education">Education</CustomLink>
-                <CustomLink to="/Skills">Skills</CustomLink>
-            </ul>
-        </nav>
-    );
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  return (
+    <nav className={`nav ${showMenu ? "show-menu" : ""}`}>
+      <div className="menu-toggle" onClick={toggleMenu}>
+        <div className="toggle-bar"></div>
+        <div className="toggle-bar"></div>
+        <div className="toggle-bar"></div>
+      </div>
+      <ul className="menu">
+        <CustomLink to="/WorkExperience">WorkExperience</CustomLink>
+        <CustomLink to="/Education">Education</CustomLink>
+        <CustomLink to="/Skills">Skills</CustomLink>
+      </ul>
+    </nav>
+  );
 }
 
 function CustomLink({ to, children }) {
