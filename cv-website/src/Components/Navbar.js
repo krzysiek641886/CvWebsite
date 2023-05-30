e1104fcacf17880b7fbed22cde3896178ec6c63a
krzysiek641886 <krzysiek6400@gmail.com>
2023-05-30 18:14:38 +0200

    Made Navbar responsive

--- cv-website/src/Components/Navbar.js
+++ cv-website/src/Components/Navbar.js
@@ -3,26 +3,22 @@ import "./Navbar.css";
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
 function Navbar() {
-  const [showMenu, setShowMenu] = useState(false);
+    const [showMenu, setShowMenu] = useState(false);
 
-  const toggleMenu = () => {
-    setShowMenu(!showMenu);
-  };
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    };
 
-  return (
-    <nav className={`nav ${showMenu ? "show-menu" : ""}`}>
-      <div className="menu-toggle" onClick={toggleMenu}>
-        <div className="toggle-bar"></div>
-        <div className="toggle-bar"></div>
-        <div className="toggle-bar"></div>
-      </div>
-      <ul className="menu">
-        <CustomLink to="/WorkExperience">WorkExperience</CustomLink>
-        <CustomLink to="/Education">Education</CustomLink>
-        <CustomLink to="/Skills">Skills</CustomLink>
-      </ul>
-    </nav>
-  );
+    return (
+        <nav className="nav">
+            <button className="menu-toggle" onClick={toggleMenu}>=</button>
+            <ul className={`horizontal-menu ${showMenu ? "show-menu" : ""}`}>
+                <CustomLink to="/WorkExperience">WorkExperience</CustomLink>
+                <CustomLink to="/Education">Education</CustomLink>
+                <CustomLink to="/Skills">Skills</CustomLink>
+            </ul>
+        </nav>
+    );
 }
 
 function CustomLink({ to, children }) {
