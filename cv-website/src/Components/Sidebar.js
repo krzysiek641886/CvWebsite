0e76ad86db8826413dd94bf8bf587279561537b3
krzysiek641886 <krzysiek6400@gmail.com>
2023-05-30 20:56:43 +0200

    Routing depends on the screen size

--- cv-website/src/Components/Sidebar.js
+++ cv-website/src/Components/Sidebar.js
@@ -1,32 +1,10 @@
 import "./Sidebar.css";
-import portrait from "../Assets/Images/portrait.jpg"; // Import your image here
-import contactInformation from "../Assets/Text/Input/contact_data.json";
-
-function printLinks(params) {
-    if (!params) return;
-
-    return (
-        <div>
-            {params.map((project, index) => (
-                <div key={index}>
-                    <a href={project.url}>{project.title}</a>
-                </div>
-            ))}
-        </div>
-    );
-}
+import PersonProfile from "./PersonProfile";
 
 function Sidebar() {
     return (
         <nav className="sidebar">
-            <div className="portrait-component">
-                <img src={portrait} alt="portrait" />
-            </div>
-            <div className="sidebar-text">
-                <h2>{contactInformation.name}</h2>
-                <p>{contactInformation.about_me}</p>
-                {printLinks(contactInformation.links)}
-            </div>
+            <PersonProfile />
         </nav>
     );
 }
---
+++ cv-website/src/Pages/ProfilePage.js
@@ -0,0 +1,11 @@
+import PersonProfile from "../Components/PersonProfile";
+
+function ProfilePage() {
+    return (
+        <div>
+            <PersonProfile />
+        </div>
+    );
+}
+
+export default ProfilePage;
