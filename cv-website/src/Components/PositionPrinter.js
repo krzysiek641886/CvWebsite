db0acad6518954cef9204b0552e1b3b6e9e44ec8
Krzysztof Mroz <57630558+krzysiek641886@users.noreply.github.com>
2023-06-05 21:00:30 +0200

    Put individual positions into gray rounded boxes (#15)
    * Save temp work
    
    * Increase profile padding to 8%

--- cv-website/src/Components/PositionPrinter.js
+++ cv-website/src/Components/PositionPrinter.js
@@ -1,7 +1,7 @@
 function PositionPrinter(params) {
     return (
-        <div>
-            <h2 className="position-title-text">{params.position_title}</h2>
+        <div className="position-content">
+            <h3 className="position-title-text">{params.position_title}</h3>
             <a className="hyperlink" href={params.link}>{params.institution_name}</a>
             <p className="time-period-text">{params.time_period}</p>
             <p className="position-description-text">{params.description}</p>
