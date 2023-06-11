6f15ca1bf6a200338a2d0751491739e197315b0c
krzysiek641886 <krzysiek6400@gmail.com>
2023-06-11 13:57:17 +0200

    Progressing with fixing the sidebar

--- cv-website/src/Pages/ProfilePage.js
+++ cv-website/src/Pages/ProfilePage.js
@@ -2,7 +2,7 @@ import PersonProfile from "../Components/PersonProfile";
 
 function ProfilePage() {
     return (
-        <div  id="profile-page">
+        <div id="profile-page">
             <h1 className="section-title">Curriculum Vitae</h1>
             <PersonProfile />
         </div>
